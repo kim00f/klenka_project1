4874513200935b66193f85ad3d2ce7bb7ae71533
Ali Mohamed <alihashem4g@gmail.com>
2025-08-13 13:06:28 +0300

    + search with description

--- api/news2/get.js
+++ api/news2/get.js
@@ -4,7 +4,7 @@ export default async function (req, res) {
  const { user_id ,search_text} = req.body;
   try {
     const result = await pool.query(`SELECT * FROM news
-       WHERE user_id=$1  AND ($2=''OR title ILIKE '%' || $2 || '%' OR EXISTS (
+       WHERE user_id=$1  AND ($2=''OR title ILIKE '%' || $2 || '%' OR description ILIKE '%' || $2 || '%' OR  EXISTS (
              SELECT 1 
              FROM unnest(key_words) kw
              WHERE kw ILIKE '%' || $2 || '%'
@@ -12,7 +12,7 @@ export default async function (req, res) {
        ORDER BY created_at DESC`,[user_id,search_text]);
     return res.status(200).json({ news: result.rows })
   } catch (err) {
-    console.error('Error fetching news:', err)
+    console.error('Error fetching news:', err) 
     return res.status(500).json({ error: 'Internal Server Error' })
   }
 }
